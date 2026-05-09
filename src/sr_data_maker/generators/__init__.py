45c372d7962df8545e6c796fb76b1e7eb284e155
KumiXH <xhk932432553@gmail.com>
2026-05-09 20:45:45 +0800

    feat: add degradation and teacher generators

---
+++ src/sr_data_maker/generators/__init__.py
@@ -0,0 +1,4 @@
+from sr_data_maker.generators.degradation import DegradationGenerator
+from sr_data_maker.generators.teacher_sr import TeacherSRGenerator
+
+__all__ = ["DegradationGenerator", "TeacherSRGenerator"]
